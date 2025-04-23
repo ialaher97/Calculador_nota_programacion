5cd3e5d2e62dbd3e96bcbf16e0d7f9379ec865ab
ialaher97 <ivan.alarcon.herrera@gmail.com>
2025-04-23 10:14:43 +0200

    Segundo Commit

--- src/main/java/org/iesvdm/calculador_nota_programacion/CalculadorNotaProgramacion.java
+++ src/main/java/org/iesvdm/calculador_nota_programacion/CalculadorNotaProgramacion.java
@@ -2,52 +2,58 @@ package org.iesvdm.calculador_nota_programacion;
 
 import java.util.Scanner;
 
-
-
-
 public class CalculadorNotaProgramacion {
-	
+	public static void main() throws Exception {
+
+		double nota1 = pedirNota();
+		double nota2 = pedirNota();
+
+		double media = media(nota1, nota2);
+
+		double notaFinal = notaFinal(media);
+		System.out.println(notaFinal);
+
+	}
+
 	public static double pedirNota() throws Exception {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduzca nota");
-		String notaSrt = sc.nextLine();
+		String notaSrt = sc.next();
 		double nota = Double.parseDouble(notaSrt);
-		
-		if(nota < 0 || nota >10) {
+
+		if (nota < 0 || nota > 10) {
 			throw new Exception("Nota no válida");
 		}
-		
-		
-	return nota;	
-	}
-	
-	public static double media(double nota1, double nota2) {
-	
-		return (nota1+nota2)/2;
-		
+
+		return nota;
 	}
 
-	
+	public static double media(double nota1, double nota2) {
+
+		return (nota1 + nota2) / 2;
+
+	}
+
 	public static double notaFinal(double media) throws Exception {
 		Scanner sc = new Scanner(System.in);
 		double notaFinal = 0.0;
-		
-		if(media<5) {
-			//pido recuperación apto o no apto.
+
+		if (media < 5) {
+			// pido recuperación apto o no apto.
 			System.out.println("¿Cual ha sido el resultado de la recuperación? apto/no apto");
-			String respuesta = sc.next();
-			
-			if(respuesta.equalsIgnoreCase("apto")) {
+			String respuesta = sc.nextLine();
+
+			if (respuesta.equalsIgnoreCase("apto")) {
 				notaFinal = 5;
-		}else if(respuesta.equalsIgnoreCase(respuesta)) {
-			notaFinal = media;
-		}else {
-			throw new Exception("Respuesta de recuperación no contemplada: "+respuesta);
+			} else if (respuesta.equalsIgnoreCase("no apto")) {
+				notaFinal = media;
+			} else {
+				throw new Exception("Respuesta de recuperación no contemplada: " + respuesta);
+			}
+
 		}
-	
-		}
-		
-		
+
 		return notaFinal;
 	}
+
 }
--- src/test/java/org/iesvdm/calculador_nota_programacion/CalculadorNotaProgramacionTest.java
+++ src/test/java/org/iesvdm/calculador_nota_programacion/CalculadorNotaProgramacionTest.java
@@ -1,56 +1,111 @@
 package org.iesvdm.calculador_nota_programacion;
 
-
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculadorNotaProgramacionTest {
-	
-	void provideInput(String data) {
-	    ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
-	    System.setIn(testIn);
+	private final PrintStream standardOut = System.out;
+
+	private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+	@Before
+	public void setUp() {
+		System.setOut(new PrintStream(outputStreamCaptor));
 	}
-	
+
+	void provideInput(String data) {
+		ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+		System.setIn(testIn);
+	}
+
 	@Test
 	public void pedirNota() throws Exception {
 		provideInput("6.5");
-		
+
 		double nota = CalculadorNotaProgramacion.pedirNota();
-		
-		Assert.assertEquals(6.5, nota,0.01);
+
+		Assert.assertEquals(6.5, nota, 0.01);
 	}
-	
-	@Test (expected = Exception.class)
+
+	@Test(expected = Exception.class)
 	public void pedirNotaFueraRango() throws Exception {
 		provideInput("11");
-		
+
 		double nota = CalculadorNotaProgramacion.pedirNota();
-		
-		Assert.assertEquals(6.5, nota,0.01);
+
+		Assert.assertEquals(6.5, nota, 0.01);
 	}
-	
+
 	@Test
 	public void pedirNotaFueraRangoTryCatch() throws Exception {
 		provideInput("11");
-		
+
 		try {
-		double nota = CalculadorNotaProgramacion.pedirNota();
-		}catch(Exception e){
-		Assert.assertEquals("Nota no válida",e.getMessage());
+			double nota = CalculadorNotaProgramacion.pedirNota();
+		} catch (Exception e) {
+			Assert.assertEquals("Nota no válida", e.getMessage());
 		}
-		}
-	
+	}
+
 	@Test
 	public void pedirNotaFueraRangoTryCatch1() throws Exception {
 		provideInput("abc");
-		
+
 		try {
-		double nota = CalculadorNotaProgramacion.pedirNota();
-		}catch(NumberFormatException e){
-		Assert.assertEquals("For input string: \"abc\"", e.getMessage());
+			double nota = CalculadorNotaProgramacion.pedirNota();
+		} catch (NumberFormatException e) {
+			Assert.assertEquals("For input string: \"abc\"", e.getMessage());
 		}
-		}
+	}
+
+	@Test
+	public void mediaTest() throws Exception {
+
+		provideInput("6");
+		double nota1 = CalculadorNotaProgramacion.pedirNota();
+		provideInput("4");
+		double nota2 = CalculadorNotaProgramacion.pedirNota();
+
+		Assert.assertEquals(6.0, nota1, 0.01);
+		Assert.assertEquals(4.0, nota2, 0.01);
+
+		double media = CalculadorNotaProgramacion.media(nota1, nota2);
+
+		Assert.assertEquals(5.0, media, 0.01);
+
+	}
+
+	@Test
+	public void aptoTest() throws Exception {
+
+		provideInput("6");
+		double nota1 = CalculadorNotaProgramacion.pedirNota();
+		provideInput("3");
+		double nota2 = CalculadorNotaProgramacion.pedirNota();
+
+		Assert.assertEquals(6.0, nota1, 0.01);
+		Assert.assertEquals(3.0, nota2, 0.01);
+
+		double media = CalculadorNotaProgramacion.media(nota1, nota2);
+
+		Assert.assertEquals(4.5, media, 0.01);
+
+		provideInput("Apto");
+		double notaFinal = CalculadorNotaProgramacion.notaFinal(media);
+
+		Assert.assertEquals(5.0, notaFinal, 0.01);
+	}
+
+	@Test
+	public void mainTest() throws Exception {
+		provideInput("6\n3\napto\n");
+		CalculadorNotaProgramacion.main();
+
+	}
 
 }
